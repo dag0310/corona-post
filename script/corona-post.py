e740cb1a6ba01ef4e1b8668f293dd0707d19427d
Daniel Geymayer <dag0310@gmail.com>
2021-01-16 20:38:43 +0100

    Update script so it takes into account receivers.txt

--- script/corona-post.py
+++ script/corona-post.py
@@ -7,37 +7,59 @@ due to COVID-19.
 import os
 import sys
 import re
+import csv
+import configparser
 import urllib.request
 import urllib.parse
 import urllib.error
 
-if len(sys.argv) < 2:
-    print('Supply country name in German as first argument (case-insensitive).')
-if len(sys.argv) < 3:
-    print('Supply type (Brief|Paket) as second argument (case-sensitive).')
-    quit()
+environment = sys.argv[1] if len(sys.argv) > 1 else 'production'
 
-country = sys.argv[1]
-type = sys.argv[2]
+config = configparser.ConfigParser()
+config.read(os.path.dirname(__file__) + os.path.sep + 'config.ini')
 
+types = ['Brief', 'Paket']
 web_url = 'https://www.post.at/p/c/liefereinschraenkungen-coronavirus'
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-csv_url_regex = r'(https://assets.post.at/-/media/Dokumente/Corona/([0-9]{8})-Annahmestopp-' + type + 'International.csv)'
 csv_charset = 'cp1252'
-email_text = None
-matches = []
 
-try:
-    req = urllib.request.Request(web_url, data=None, headers={'User-Agent': user_agent})
-    matches = re.findall(csv_url_regex, str(urllib.request.urlopen(req).read()))
-except urllib.error.HTTPError as error:
-    email_text = 'HTTP error "' + str(error.code) + '", reason: "' + error.reason + '". Could not fetch web page ' + web_url
-    print(email_text)
-except:
-    email_text = 'Unknown error when trying to fetch web page ' + web_url
-    print(email_text)
+def send_email(name, email, message):
+    data = urllib.parse.urlencode({
+        'secret': config.get('mailer', 'secret'),
+        'subject': 'corona-post',
+        'name': name,
+        'email': email,
+        'message': message,
+    })
+    try:
+        urllib.request.urlopen(urllib.request.Request(config.get('mailer', 'api_url'), data=data.encode()))
+        print('Email sent to "' + email + '"')
+    except Exception as error:
+        print(error)
+        print('Email could NOT be sent to "' + email + '"')
 
-if len(matches) > 0:
+def send_admin_email(message):
+    print('Sending admin email: ' + message)
+    send_email(config.get('mailer', 'name'), config.get('mailer', 'email'), message)
+
+def notify_receivers(receivers, type, csv_url_regex):
+    matches = []
+    try:
+        req = urllib.request.Request(web_url, data=None, headers={'User-Agent': user_agent})
+        matches = re.findall(csv_url_regex, str(urllib.request.urlopen(req).read()))
+    except urllib.error.HTTPError as error:
+        print(error)
+        send_admin_email('HTTP error "' + str(error.code) + '", reason: "' + error.reason + '". Could not fetch web page ' + web_url)
+        quit()
+    except Exception as error:
+        print(error)
+        send_admin_email('Unknown error when trying to fetch web page ' + web_url)
+        quit()
+
+    if len(matches) <= 0:
+        send_admin_email('Could not find a CSV download URL on the web page ' + web_url)
+        quit()
+
     csv_url = matches[0][0]
     date_str_raw = matches[0][1]
     date_str = date_str_raw[0:4] + '-' + date_str_raw[4:6] + '-' + date_str_raw[6:]
@@ -45,36 +67,35 @@ if len(matches) > 0:
         print('Downloading ' + csv_url)
         req = urllib.request.Request(csv_url, data=None, headers={'User-Agent': user_agent})
         csv_text = str(urllib.request.urlopen(req).read().decode(csv_charset))
-        country_blocked = country.lower() in csv_text.strip().lower()
-        print('Country "' + country + '" blocked: ' + str(country_blocked))
-        if not country_blocked:
-            email_text = 'You can send your package to "' + country + '" as of "' + date_str + '"\n\nCheck for yourself here: ' + web_url
+        for receiver in receivers:
+            if receiver['type'] != type:
+                continue
+            country_blocked = receiver['country'].strip().lower() in csv_text.strip().lower()
+            if environment != 'production':
+                print('Country "' + receiver['country'] + '" blocked: ' + str(country_blocked))
+            if not country_blocked:
+                message = 'Du kannst dein ' + type + ' nach ' + receiver['country'] + ' schicken seit ' + date_str + '.\n\nGeschickt von https://apps.geymayer.com/corona-post'
+                send_email(receiver['email'], receiver['email'], message)
     except urllib.error.HTTPError as error:
-        print('Uh oh... could not fetch CSV file. code "' + str(error.code) + '", reason: "' + error.reason + '"')
-        email_text = 'HTTP error "' + str(error.code) + '", reason: "' + error.reason + '". Could not fetch CSV file with country "' + country + '" for date ' + date_str + ' from URL: ' + csv_url + '\n\nPlease check ' + web_url
-    except:
-        print('Some weird error occurred.')
-        email_text = 'Some weird error occurred when reading CSV file with country "' + country + '".'
-else:
-    email_text = 'Could not find a CSV download URL on the web page ' + web_url
+        print(error)
+        send_admin_email('HTTP error "' + str(error.code) + '", reason: "' + error.reason + '". Could not fetch CSV file for date ' + date_str + ' from URL: ' + csv_url + '\n\nPlease check ' + web_url)
+        quit()
+    except Exception as error:
+        print(error)
+        send_admin_email('Some weird error occurred when reading CSV file ' + csv_url)
+        quit()
 
-if email_text is None:
-    print('No email needed to be sent.')
-else:
-    import configparser
-    config = configparser.ConfigParser()
-    config.read(os.path.dirname(__file__) + os.path.sep + 'config.ini')
-    data = urllib.parse.urlencode({
-        'secret': config.get('mailer', 'secret'),
-        'name': config.get('mailer', 'name'),
-        'email': config.get('mailer', 'email'),
-        'subject': 'corona-post',
-        'message': email_text
-    })
-    try:
-        urllib.request.urlopen(urllib.request.Request(config.get('mailer', 'api_url'), data=data.encode()))
-        print('Email sent.')
-    except:
-        print('Email could NOT be sent.')
+def main():
+    receivers = []
+    with open(config.get(environment, 'receivers_path'), newline='\n') as receivers_file:
+        for row in csv.reader(receivers_file, delimiter='\t'):
+            receivers.append({ 'email': row[0], 'country': row[1], 'type': row[2] })
 
-print('Script finished.')
+    for type in types:
+        notify_receivers(receivers, type, r'(https://assets.post.at/-/media/Dokumente/Corona/([0-9]{8})-Annahmestopp-' + type + 'International.csv)')
+
+    print('Script finished.')
+    return 0
+
+if __name__ == "__main__":
+    main()
